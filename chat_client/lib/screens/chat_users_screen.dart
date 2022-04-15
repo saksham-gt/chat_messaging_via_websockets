d2fb5775369af46f300728ff6fd60fbc181924d8
Saksham Gupta <sakshamgupta391@gmail.com>
2022-04-15 16:38:32 +0530

    added subscription feature

--- chat_client/lib/screens/chat_users_screen.dart
+++ chat_client/lib/screens/chat_users_screen.dart
@@ -1,6 +1,8 @@
 import 'package:chat_client/global.dart';
+import 'package:chat_client/razorpay_key.dart';
 import 'package:chat_client/screens/conversation_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 
 class ChatUsersScreen extends StatefulWidget {
   static const routeName = '/chat-users-screen';
@@ -11,6 +13,8 @@ class ChatUsersScreen extends StatefulWidget {
 }
 
 class _ChatUsersScreenState extends State<ChatUsersScreen> {
+  final Razorpay _razorpay = Razorpay();
+
   initSocket() async {
     Global.initSocket();
     await Global.socket!.initSocket(Global.currentUser!);
@@ -40,6 +44,7 @@ class _ChatUsersScreenState extends State<ChatUsersScreen> {
   @override
   void dispose() {
     Global.socket!.closeConnection();
+    if (_razorpay != null) _razorpay.clear();
     super.dispose();
   }
 
@@ -48,6 +53,56 @@ class _ChatUsersScreenState extends State<ChatUsersScreen> {
     initSocket();
     super.initState();
   }
+
+  @override
+  void didChangeDependencies() {
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _paymentSuccessHandler);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _paymentErrorHandler);
+
+    super.didChangeDependencies();
+  }
+
+  _paymentSuccessHandler(PaymentSuccessResponse success) {
+    print('Payment Success : $success');
+    return ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Row(children: const [
+        SizedBox(width: 50),
+        Icon(
+          Icons.check_circle_outline_rounded,
+          color: Colors.black,
+        ),
+        SizedBox(width: 50),
+        Expanded(child: Text('Hooray!! You\'re subscribed!!'))
+      ]),
+      backgroundColor: Colors.green[100],
+    ));
+  }
+
+  _paymentErrorHandler(PaymentFailureResponse failure) {
+    print('Payment Failure: $failure');
+
+    return ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+      content: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+        child: const ListTile(
+            leading: Icon(
+              Icons.close_rounded,
+              color: Colors.black,
+            ),
+            title: Text('Oops!! Error processing payment.')),
+      ),
+      backgroundColor: Colors.red[100],
+    ));
+  }
+
+  Map<String, dynamic> options = {
+    'key': RazorPayCreds.testKey, // <YOUR_RAZORPAY_KEY>
+    'amount': 100,
+    'name': 'ChatMessaging',
+    'description': 'Subscription to Chat',
+    'prefill': {'contact': '9952632252', 'email': 'test@razorpay.com'},
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -76,6 +131,22 @@ class _ChatUsersScreenState extends State<ChatUsersScreen> {
         ),
         itemCount: Global.dummyChatUsers!.length,
       ),
+      floatingActionButton: TextButton(
+          style: ButtonStyle(
+              padding: MaterialStateProperty.all(
+                  const EdgeInsets.symmetric(vertical: 10, horizontal: 6)),
+              backgroundColor: MaterialStateProperty.all(Colors.teal)),
+          onPressed: () async {
+            try {
+              _razorpay.open(options);
+            } catch (e) {
+              print('Error : ${e.toString()}');
+            }
+          },
+          child: const Text(
+            'Subscribe to Chat Feature',
+            style: TextStyle(color: Colors.white),
+          )),
     );
   }
 }
